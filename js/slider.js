54c268f0a750412ec7fc752c66a0c0d4ac14216d
tanvir321110 <tanvirahmed321110@gmail.com>
2024-09-02 21:33:20 +0600

    added a page allcourse .html

--- js/slider.js
+++ js/slider.js
@@ -22,3 +22,31 @@ var swiper = new Swiper('.related-items-body', {
         }
     }
 });
+
+
+
+
+var swiper2 = new Swiper(".popular-courses-area", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true, // Allows the pagination bullets to be clickable
+    },
+    slidesPerView: 1, // How many slides to show at once
+    spaceBetween: 10, // Space between each slide
+    loop: true, // Enables continuous loop mode
+    speed: 1000, // Transition speed
+    autoplay: {
+        delay: 2000, // Delay between transitions (in ms)
+        disableOnInteraction: false // Autoplay continues even when user interacts with slides
+    },
+    breakpoints: { // Responsive breakpoints
+        768: { // when window width is >= 768px
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        1024: { // when window width is >= 1024px
+            slidesPerView: 2,
+            spaceBetween: 30
+        }
+    }
+});
